1618de508696413386128a49a52a81de177594bd
FKettl <fekettl@gmail.com>
2021-11-24 19:02:47 -0300

    Estilizando todo o app

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
-import { View, Text, TextInput, Button} from "react-native"
+import { View, Text, TextInput, TouchableOpacity} from "react-native"
 import ResultImc from "./ResultImc";
+import styles from "./style";
+
 
 export default function Form() {
 
@@ -32,25 +34,31 @@ function validationImc(){
 }
 
     return(
-        <View>
-             <View>
-                <Text>Altura</Text>
+        <View style={styles.formContext}>
+             <View style={styles.form}> 
+                <Text style={styles.formLabel}>Altura</Text>
                 <TextInput
+                style={styles.input}
                 onChangeText={setHeight}
                 value={height}
                 placeholder= "Ex. 1.75"
                 keyboardType="numeric"
                 ></TextInput>
-                <Text>Peso</Text>
-                <TextInput
+                <Text style={styles.formLabel}>Peso</Text>
+                <TextInput 
+                style={styles.input}
                 onChangeText={setWeight}
                 value={weight}
                 placeholder= "Ex. 75.5"
                 keyboardType="numeric"
                 ></TextInput>
-                <Button 
-                onPress={() => validationImc()}
-                title={textButton}/>
+                <TouchableOpacity
+                style={styles.buttonCalculator}
+                onPress= {() => {
+                    validationImc()
+                }}>
+                <Text style={styles.textButtonCalculator}>{textButton}</Text>
+                </TouchableOpacity>
              </View>
              <ResultImc messageResultImc = {messageImc} resultImc={imc}/>
         </View>
